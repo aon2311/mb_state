0dc7b33f470b64467f3d9408e86c7331457bba72
aon2311 <aononwara39@gmail.com>
2026-01-05 10:17:50 +0700

    gay

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -8,38 +8,62 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
-  String _output ='';
+  String _output = '';//1
+  final _textCotroller = TextEditingController();//2
+
+  @override
+  void initState() {
+    super.initState();
+    debugPrint("initState");
+    _output='Enter Your Name';
+  }
+
+  @override
+  void dispose() {
+   _textCotroller.dispose();
+   super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('MyApp CS KMUTNB'),
-        backgroundColor: Colors.green[200]
-            
-      ),
+          title: const Text('MyApp CS KMUTNB'),
+          backgroundColor: Colors.green[200]),
       body: Center(
-        child:Column(
-          children: [
-            Text('Hello Bro',
-            style: TextStyle(fontSize: 25 , fontWeight: FontWeight.bold),
+          child: Column(
+        children: [
+          Text(
+            'Hello Bro',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          TextField(
+            controller: _textCotroller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Ennter Name',
+              labelText: "Name",
             ),
-            Text(
-              '${_output}',
-              style: TextStyle(fontSize: 18),
-            ),
-            SizedBox(height: 10,),
-            ElevatedButton(onPressed: () {
-              debugPrint('woowww ');
-              setState(() {
-                _output= 'Aon Onwara';
-              });
-            }, child: Text('click'))
-          ],
-          
-        )
-      ),
-        
+          ),
+          Text(
+            '${_output}',
+            style: TextStyle(fontSize: 18),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                String input = _textCotroller.text;
+                debugPrint('Input : $input ');
+                setState(() {
+                  _output = 'my name is $input';
+                });
+              },
+              child: Text('click'))
+        ],
+      )),
     );
   }
 }
