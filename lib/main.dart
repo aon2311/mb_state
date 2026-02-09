8a355deaafce6cdbabc084e70c72987dacdfe409
Aonos2311 <aononwara39@gmail.com>
2026-02-09 10:06:48 +0700

    week9_2

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/about_page.dart';
-import 'package:myapp/detail.dart';
+import 'package:myapp/detail_page.dart';
 import 'package:myapp/display_page.dart';
-import 'package:myapp/list_page.dart';
+
 import 'package:myapp/model/product.dart';
 
 void main() {
@@ -18,13 +18,11 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
-        '/': (context) => const ListPage(),
+        //'/': (context) => const AboutPage(),
+        '/': (context) => const DetailPage(),
         '/about': (context) => const AboutPage(),
         '/display': (context) => const DisplayPage(name: ''),
-        '/detail': (context) {
-          final product = ModalRoute.of(context)!.settings.arguments as Product;
-          return Detail(product: product);
-        },
+        
       },
     );
   }
